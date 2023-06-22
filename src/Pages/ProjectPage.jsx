c7e42bef5bfbfec61274867d76ffe7d9ca7987de
JoshM1014 <Joshaumartinez1014@gmail.com>
2023-06-22 18:53:55 -0500

    ProjectPage: includes 2 projects displayed

--- src/Pages/ProjectPage.jsx
+++ src/Pages/ProjectPage.jsx
@@ -1,6 +1,7 @@
 import { Container, Form, Button } from "react-bootstrap";
 import { Table } from "react-bootstrap";
 import gradebook from "../img/gradebook-screenshot.png";
+import techblog from "../img/tech-blog.png";
 import "../styles/global.css";
 
 function ProjectPage() {
@@ -29,6 +30,8 @@ function ProjectPage() {
               models used in the creation of this application: User, Grade,
               Class, and Assignment.
             </p>
+            <button className="main-content-button">GitHub</button>
+            <button className="main-content-button">Live Site</button>
           </p>
         </div>
         {/*  */}
@@ -58,21 +61,36 @@ function ProjectPage() {
             }}
           >
             <div style={{ fontSize: "3rem", lineHeight: "60px" }}>
-              <strong>Gradebook</strong>
+              <strong>Tech Blog</strong>
             </div>
             <br />
             <p style={{ fontSize: "17px" }}>
-              &nbsp;&nbsp;&nbsp;&nbsp;This application exhibits a student's
-              grades in a simple and organized manner. The overall goal of the
-              application is to organize grade information in a way that makes
-              sense and can be quickly viewed by users. Grades can be seen by a
-              user that is a teacher or a student. New users must sign up to
-              access most of the application and returning users can log in. The
-              server-side code is heavily dependent on Express, MongoDB, and
-              Mongoose while the client-side mainly uses React. There are four
-              models used in the creation of this application: User, Grade,
-              Class, and Assignment.
+              &nbsp;&nbsp;&nbsp;&nbsp;Create a site where users can come
+              together and discuss relevent tech information in the form of
+              blogging. The user can make their own posts, or comment on other
+              posts. The site will be able to keep user information by allowing
+              users to create accounts and logging into them.
             </p>
+            <button
+              className="main-content-button"
+              style={{
+                color: "rgb(17,180,242)",
+                backgroundColor: "white",
+                border: "solid 2px rgb(17,180,242)",
+              }}
+            >
+              GitHub
+            </button>
+            <button
+              className="main-content-button"
+              style={{
+                color: "rgb(17,180,242)",
+                backgroundColor: "white",
+                border: "solid 2px rgb(17,180,242)",
+              }}
+            >
+              Live Site
+            </button>
           </p>
         </div>
         {/*  */}
@@ -80,10 +98,7 @@ function ProjectPage() {
           className="homepage-section"
           style={{ marginTop: "40px", width: "50%" }}
         >
-          <img
-            src={gradebook}
-            style={{ height: "350px", width: "700px" }}
-          ></img>
+          <img src={techblog} style={{ height: "375px", width: "700px" }}></img>
         </div>
         {/*  */}
       </div>
