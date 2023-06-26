4323a60dcd3654966773f7947ca76f3d275c0ad1
JoshM1014 <Joshaumartinez1014@gmail.com>
2023-06-26 00:45:32 -0500

    modified server file

--- server.js
+++ server.js
@@ -13,7 +13,17 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  // Any route that gets to this point will be redirected to your React app
+  app.get("*", function (req, res) {
+    res.sendFile(
+      path.join(__dirname, "../client/build/index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
+  });
 }
 
 app.use(routes);
