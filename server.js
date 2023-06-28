bd5bbd16d40ee75bfcc98f891c55c92ae6abb2e7
JoshM1014 <Joshaumartinez1014@gmail.com>
2023-06-28 08:57:25 -0500

    modify server.js

--- server.js
+++ server.js
@@ -11,19 +11,6 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../build")));
-  // Any route that gets to this point will be redirected to your React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../build/index.html"), function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    });
-  });
-}
-
 app.use(routes);
 
 db.once("open", () => {
